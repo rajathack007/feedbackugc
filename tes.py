828d48da16f52311001cf4aab188dd7403abf274
Rajat Kumar <33064174+rajathack007@users.noreply.github.com>
2022-07-24 01:24:22 +0530

    Add files via upload

---
+++ tes.py
@@ -0,0 +1,51 @@
+import pywhatkit
+from datetime import datetime
+dt = datetime.now()
+print('Datetime is:', dt)
+day= dt.strftime('%A')
+print('Day of a week is:', day)
+
+if (day=="Monday"):
+        print("Today day is: ",day)
+        pywhatkit.sendwhatmsg('+918607821560','kindly fill the feedback form of EOP https://docs.google.com/spreadsheets/d/1c0sQEBkWzOzx1EcNCaPNt92UlFSaNM4gSeAXyVW8L9Q/edit#gid=0',20,30)
+        pywhatkit.sendwhatmsg('+918607821560','kindly fill the feedback form of EOP https://docs.google.com/spreadsheets/d/1c0sQEBkWzOzx1EcNCaPNt92UlFSaNM4gSeAXyVW8L9Q/edit#gid=0',21,00)
+elif (day=="Tuesday"):
+        print("Today day is: ",day)
+        pywhatkit.sendwhatmsg('+918607821560','kindly fill the feedback form of FSD https://docs.google.com/spreadsheets/d/1c0sQEBkWzOzx1EcNCaPNt92UlFSaNM4gSeAXyVW8L9Q/edit#gid=0',20,30)
+        pywhatkit.sendwhatmsg('+918607821560','kindly fill the feedback form of FSD https://docs.google.com/spreadsheets/d/1c0sQEBkWzOzx1EcNCaPNt92UlFSaNM4gSeAXyVW8L9Q/edit#gid=0',21,00)
+elif (day=="Wednesday"):
+        print("Today day is: ",day)
+        pywhatkit.sendwhatmsg('+918607821560','kindly fill the feedback form of EOP https://docs.google.com/spreadsheets/d/1c0sQEBkWzOzx1EcNCaPNt92UlFSaNM4gSeAXyVW8L9Q/edit#gid=0',20,30)
+        pywhatkit.sendwhatmsg('+918607821560','kindly fill the feedback form of EOP https://docs.google.com/spreadsheets/d/1c0sQEBkWzOzx1EcNCaPNt92UlFSaNM4gSeAXyVW8L9Q/edit#gid=0',21,00)
+elif (day=="Thursday"):
+        print("Today day is: ",day)
+        pywhatkit.sendwhatmsg('+918607821560','kindly fill the feedback form of FSD https://docs.google.com/spreadsheets/d/1c0sQEBkWzOzx1EcNCaPNt92UlFSaNM4gSeAXyVW8L9Q/edit#gid=0',20,30)
+        pywhatkit.sendwhatmsg('+918607821560','kindly fill the feedback form of FSD https://docs.google.com/spreadsheets/d/1c0sQEBkWzOzx1EcNCaPNt92UlFSaNM4gSeAXyVW8L9Q/edit#gid=0',21,00)
+elif (day=="Friday"):
+        print("Today day is: ",day)
+        pywhatkit.sendwhatmsg('+918607821560','kindly fill the feedback form of EOP https://docs.google.com/spreadsheets/d/1c0sQEBkWzOzx1EcNCaPNt92UlFSaNM4gSeAXyVW8L9Q/edit#gid=0',20,30)
+        pywhatkit.sendwhatmsg('+918607821560','kindly fill the feedback form of EOP https://docs.google.com/spreadsheets/d/1c0sQEBkWzOzx1EcNCaPNt92UlFSaNM4gSeAXyVW8L9Q/edit#gid=0',21,00)
+elif (day=="Saturday"):
+        print("Today day is: ",day)
+        pywhatkit.sendwhatmsg('+918607821560','kindly fill the feedback form of FSD https://docs.google.com/spreadsheets/d/1c0sQEBkWzOzx1EcNCaPNt92UlFSaNM4gSeAXyVW8L9Q/edit#gid=0',20,30)
+        pywhatkit.sendwhatmsg('+918607821560','kindly fill the feedback form of FSD https://docs.google.com/spreadsheets/d/1c0sQEBkWzOzx1EcNCaPNt92UlFSaNM4gSeAXyVW8L9Q/edit#gid=0',21,00)
+        pywhatkit.sendwhatmsg('+918607821560','kindly fill the feedback form of EOP https://docs.google.com/spreadsheets/d/1c0sQEBkWzOzx1EcNCaPNt92UlFSaNM4gSeAXyVW8L9Q/edit#gid=0',21,00)
+        pywhatkit.sendwhatmsg('+918607821560','kindly fill the feedback form of Cloud https://docs.google.com/spreadsheets/d/1c0sQEBkWzOzx1EcNCaPNt92UlFSaNM4gSeAXyVW8L9Q/edit#gid=0',20,30)
+        pywhatkit.sendwhatmsg('+918607821560','kindly fill the feedback form of Cloud https://docs.google.com/spreadsheets/d/1c0sQEBkWzOzx1EcNCaPNt92UlFSaNM4gSeAXyVW8L9Q/edit#gid=0',21,00)
+        pywhatkit.sendwhatmsg('+918607821560','kindly fill the feedback form of EOP https://docs.google.com/spreadsheets/d/1c0sQEBkWzOzx1EcNCaPNt92UlFSaNM4gSeAXyVW8L9Q/edit#gid=0',20,30)
+        pywhatkit.sendwhatmsg('+918607821560','kindly fill the feedback form of CU PYTHONhttps://docs.google.com/spreadsheets/d/1c0sQEBkWzOzx1EcNCaPNt92UlFSaNM4gSeAXyVW8L9Q/edit#gid=0',20,30)
+        pywhatkit.sendwhatmsg('+918607821560','kindly fill the feedback form of CU PYTHON https://docs.google.com/spreadsheets/d/1c0sQEBkWzOzx1EcNCaPNt92UlFSaNM4gSeAXyVW8L9Q/edit#gid=0',21,00)
+elif (day=="Sunday"):
+        print("Today day is: ",day)
+        
+        pywhatkit.sendwhatmsg('+918607821560','kindly fill the feedback form of FSD https://docs.google.com/spreadsheets/d/1c0sQEBkWzOzx1EcNCaPNt92UlFSaNM4gSeAXyVW8L9Q/edit#gid=0',1,3)
+        pywhatkit.sendwhatmsg('+918607821560','kindly fill the feedback form of FSD https://docs.google.com/spreadsheets/d/1c0sQEBkWzOzx1EcNCaPNt92UlFSaNM4gSeAXyVW8L9Q/edit#gid=0',21,00)
+        pywhatkit.sendwhatmsg('+918607821560','kindly fill the feedback form of EOP https://docs.google.com/spreadsheets/d/1c0sQEBkWzOzx1EcNCaPNt92UlFSaNM4gSeAXyVW8L9Q/edit#gid=0',00,40)
+        pywhatkit.sendwhatmsg('+918607821560','kindly fill the feedback form of Cloud https://docs.google.com/spreadsheets/d/1c0sQEBkWzOzx1EcNCaPNt92UlFSaNM4gSeAXyVW8L9Q/edit#gid=0',20,30)
+        pywhatkit.sendwhatmsg('+918607821560','kindly fill the feedback form of EOP https://docs.google.com/spreadsheets/d/1c0sQEBkWzOzx1EcNCaPNt92UlFSaNM4gSeAXyVW8L9Q/edit#gid=0',20,30)
+        pywhatkit.sendwhatmsg('+918607821560','kindly fill the feedback form of CU PYTHONhttps://docs.google.com/spreadsheets/d/1c0sQEBkWzOzx1EcNCaPNt92UlFSaNM4gSeAXyVW8L9Q/edit#gid=0',20,30)
+        pywhatkit.sendwhatmsg('+918607821560','kindly fill the feedback form of CU PYTHONhttps://docs.google.com/spreadsheets/d/1c0sQEBkWzOzx1EcNCaPNt92UlFSaNM4gSeAXyVW8L9Q/edit#gid=0',21,00)
+        pywhatkit.sendwhatmsg('+918607821560','kindly fill the feedback form of CLOUD https://docs.google.com/spreadsheets/d/1c0sQEBkWzOzx1EcNCaPNt92UlFSaNM4gSeAXyVW8L9Q/edit#gid=0',21,00)
+else :
+    print("Have a Nice Day")
+    pywhatkit.sendwhatmsg('+918607821560','No Session Today, Lets Chill',20,30)
